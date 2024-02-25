6c99e93525a16f95006bbc9ac6bc8069ec6ef970
Carter Walsh <cartwalsh@gmail.com>
2024-02-24 23:18:59 -0600

    adding lab??

--- src/components/ArcticLab.tsx
+++ src/components/ArcticLab.tsx
@@ -1,16 +1,17 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import styles from "../app/page.module.css"; // ensure this path is correct
+import styles from "../app/page.module.css";
+import {Box, Heading} from "@chakra-ui/react"; // ensure this path is correct
 
 export default function ArcticLab() {
     const [fileTree, setFileTree] = useState({});
 
-    useEffect( () => {
+    useEffect(() => {
         // Fetch the file list from the API
         const fetchData = async () => {
             try {
-                const response = await fetch('../pages/api/files');
+                const response = await fetch('/api/files'); // Use the correct API endpoint
                 if (!response.ok) {
                     throw new Error(`Error: ${response.statusText}`);
                 }
@@ -22,12 +23,17 @@ export default function ArcticLab() {
         };
 
         fetchData();
-    }, [fileTree]);
+    }, []); //
 
     return (
         <main className={styles.main}>
             <h1>This is the lab</h1>
-            <pre>{JSON.stringify(fileTree, null, 2)}</pre>
+            {/* Render the file tree or a message if it's empty */}
+            {Object.keys(fileTree).length ? (
+                <pre>{JSON.stringify(fileTree, null, 2)}</pre>
+            ) : (
+                <p>No files to display.</p>
+            )}
         </main>
     );
 }
--- src/pages/api/files.ts
+++ src/pages/api/files.ts
@@ -4,11 +4,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 import path from 'path';
 
-const smbSharePath = '/arcticlab'; // The path to your SMB share
+const smbSharePath = '/arctic_lab'; // The path to your SMB share
+
+interface DirectoryResult {
+    [key: string]: string | DirectoryResult;
+}
 
 // A function to recursively read a directory
-function readDirectory(dir: string): any {
-    const result: { [key: string]: any } = {};
+function readDirectory(dir: string): DirectoryResult {
+    const result: DirectoryResult = {};
     fs.readdirSync(dir).forEach(file => {
         const fullPath = path.join(dir, file);
         if (fs.statSync(fullPath).isDirectory()) {
@@ -27,6 +31,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         res.status(200).json(arcticrpi);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Error reading directory' });
+        if (error instanceof Error) {
+            res.status(500).json({ error: 'Error reading directory', message: error.message });
+        } else {
+            res.status(500).json({ error: 'Error reading directory', message: 'An unknown error occurred' });
+        }
     }
 }
