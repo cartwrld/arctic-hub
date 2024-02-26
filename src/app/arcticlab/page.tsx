9f525d5603e69d68b9e3c7d342ceecca3668eceb
Carter Walsh <cartwalsh@gmail.com>
2024-02-26 17:25:49 -0600

    logging the files to the console TERMINAL window

--- src/app/arcticlab/page.tsx
+++ src/app/arcticlab/page.tsx
@@ -1,12 +1,15 @@
 
 import styles from "../page.module.css";
-import ArcticLab from "@/components/ArcticLab"; // ensure this path is correct
+import ArcticLab from "@/components/ArcticLab";
+import {Flex} from "@chakra-ui/react"; // ensure this path is correct
 
 export default function ArcLab() {
 
     return (
         <main className={styles.main}>
-           <ArcticLab/>
+           <Flex w={'100vw'} justifyContent={'center'} alignItems={'center'}>
+               <ArcticLab/>
+           </Flex>
         </main>
     );
 }
--- src/components/ArcticLab.tsx
+++ src/components/ArcticLab.tsx
@@ -2,19 +2,8 @@
 
 import React, {useState, useEffect} from 'react';
 import styles from "../app/page.module.css"; // ensure this path is correct
-import {Box, Center, Flex, Heading, Text, VStack} from "@chakra-ui/react"; // ensure this path is correct
+import {Box, Button, Center, Flex, Heading, Text, VStack} from "@chakra-ui/react"; // ensure this path is correct
 
-interface FileDetail {
-    name: string;
-    ext: string;
-    fullpath: string;
-}
-
-interface DirDetail {
-    name: string;
-    fullpath: string;
-    children: any[]; // Adjusted for compatibility
-}
 
 export default function ArcticLab() {
     const [fileTree, setFileTree] = useState<{ [key: string]: any }>({});
@@ -39,34 +28,43 @@ export default function ArcticLab() {
     }, []);
 
     function handleTree() {
-        return Object.keys(fileTree).map((k) =>{
+        return Object.keys(fileTree).map((k) => {
 
-            const pattern = /.*\.\*$/; // This is the regex for a string ending with ".*"
+            const pattern = /.*\.\w+$/; // This is the regex for a string ending with ".*"
             let color = pattern.test(k) ? 'lightblue' : 'lightgreen'
 
-            return (<Flex justifyContent={'center'} alignItems={'center'} w={100} h={100} p={4} m={4} bg={color} key={k}>
-                {k}
-            </Flex>)
+            return (<FSItem key={k} item={k} color={color}/>)
         })
     }
 
 
     return (
-        <main className={styles.main}>
-            <Flex direction="column" p={5}>
-                <Heading mb={5}>This is the lab</Heading>
-                <Center>
-                    {
-                        Object.keys(fileTree).length > 0
-                            ? handleTree()
-                            : (<Center>No files found.</Center>)
-                    }
-                </Center>
-            </Flex>
-        </main>
+
+        <Flex direction="column" p={5}>
+            <Heading mb={5}>This is the lab</Heading>
+            <Center>
+                {
+                    Object.keys(fileTree).length > 0
+                        ? handleTree()
+                        : (<Center>No files found.</Center>)
+                }
+            </Center>
+        </Flex>
+
+    )
+
+
+function FSItem(props: {item: string, color: string}) {
+
+        const {item, color} = props
+
+    return (
+        <Button bg={color} key={item}>
+            <Flex justifyContent={'center'} alignItems={'center'} w={'fit-content'} maxW={200} h={50} p={4} m={4} >
+            {item}
+        </Flex>
+        </Button>
     )
 }
-
-
 
 
