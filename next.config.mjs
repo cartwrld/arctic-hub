a6b7819894c13980b742112010f88d2edefe57e5
Carter Walsh <cartwalsh@gmail.com>
2024-02-25 19:06:56 -0600

    adding lab??

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Convert file URL to __dirname equivalent when using ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+    webpackDevMiddleware: config => {
+        config.watchOptions.poll = 300; // Check for changes every 300ms
+        return config;
+    },
+    serverRuntimeConfig: {
+        PROJECT_ROOT: __dirname
+    },
+    // Your other Next.js configurations...
+};
 
 export default nextConfig;
