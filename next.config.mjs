512af8c0f401b1e8c02a86f6cbe48c373a937fc5
Carter Walsh <cartwalsh@gmail.com>
2024-02-25 21:44:10 -0600

    ??????

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-// Convert file URL to __dirname equivalent when using ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const nextConfig = {
-    webpackDevMiddleware: config => {
-        config.watchOptions.poll = 300; // Check for changes every 300ms
-        return config;
-    },
-    serverRuntimeConfig: {
-        PROJECT_ROOT: __dirname
-    },
-    // Your other Next.js configurations...
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
