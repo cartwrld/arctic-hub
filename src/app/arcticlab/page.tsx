6056775d378882c9e2e1d30e0f7399d7c6632970
Carter Walsh <cartwalsh@gmail.com>
2024-02-26 19:44:32 -0600

    arctic lab pic

--- src/app/arcticlab/page.tsx
+++ src/app/arcticlab/page.tsx
@@ -1,19 +1,27 @@
 import styles from "../page.module.css";
 import ArcticLab from "@/components/ArcticLab";
-import {Breadcrumb, BreadcrumbItem, BreadcrumbSeparator, Flex} from "@chakra-ui/react"; // ensure this path is correct
+import {Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, Flex} from "@chakra-ui/react"; // ensure this path is correct
 
 export default function ArcLab() {
 
     return (
         <Flex w={'100%'} h={"100%"} justifyContent={'center'} alignItems={'center'}>
             <Breadcrumb>
-                <BreadcrumbItem>root</BreadcrumbItem>
-                <BreadcrumbSeparator>/</BreadcrumbSeparator>
-                <BreadcrumbItem>torrents</BreadcrumbItem>
-                <BreadcrumbSeparator>/</BreadcrumbSeparator>
-                <BreadcrumbItem>undownloaded</BreadcrumbItem>
-                <BreadcrumbSeparator>/</BreadcrumbSeparator>
-                <BreadcrumbItem>basedir</BreadcrumbItem>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href='#'>arctic_lab</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator/>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href='#'>torrents</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator/>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href='#'>undownloaded</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator/>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href='#'>oldwhite</BreadcrumbLink>
+                </BreadcrumbItem>
             </Breadcrumb>
             <ArcticLab/>
         </Flex>
